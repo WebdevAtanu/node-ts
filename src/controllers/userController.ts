c9e6b19bb58168b18d8f46bd3793f97e752ff84e
Atanu Mondal <atanum991@gmail.com>
2026-03-18 20:03:33 +0530

    modified

---
+++ src/controllers/userController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import User from "../models/User";
+
+export const getUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+export const createUser = async (req: Request, res: Response) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ error: "Invalid data" });
+  }
+};
